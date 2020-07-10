99f2f44057f0b2d97d50bdd2cd3071223b9bac82
sangmin802 <56113148+sangmin802@users.noreply.github.com>
2020-07-10 17:52:20 +0900

    comp

--- js/Vue_component.js
+++ js/Vue_component.js
@@ -228,3 +228,237 @@ new Vue({
 })
 
 //    3) .sync 수식어
+//        자식 컴포넌트의 .sync 수식어를 붙인 속성은 부모와 양방향 바인딩이되어, 자식 컴포넌트의 값을 변경하면 부모도 변경된다.
+//        자식컴포넌트에서 해당 값을 data로 설정해 줄 필요 없으며, 변경 요청시 update:속성명으로 설정한다.
+new Vue({
+  el : '#sync',
+  data : {
+    text : 'Hello'
+  },
+  components : {
+    child : {
+      props : ['text'],
+      template : `
+        <button @click="change">{{text}}</button>
+      `,
+      methods : {
+        change(){
+          this.$emit('update:text', 'Hi')
+        }
+      }
+    }
+  }
+})
+
+//    4) 사용자 정의 이벤트를 사용하여 폼 입력 컴포넌트 만들기
+//       커스텀이벤트는 v-model 에서 작동하는 커스텀입력을 만드는데에도 사용할 수 있다.
+//       기존 v-model은 value 속성과 input 이벤트를 함께 사용하는것같다. 즉, input 이벤트가 발생했을 때, value의 값을 변경하는 것.
+
+//      설명
+//      1) v-model을 갖는 컴포넌트의 인스턴스는 template에, :value로 부모로부터 v-model로 보내진 값을 초기값으로 설정하며, @input이라는 커스텀이벤트를 생성할 수 있는데, 해당 커스텀이벤트로 input의 value를 조회하여 부모에게 방출해줄 수 있다.
+new Vue({
+  el : '#v-model1',
+  data : {
+    text : '초기값'
+  },
+  components : {
+    'custom-input' : {
+      props : ['value'],
+      template : `
+        <input type="text"
+          :value="value" 
+          @input="inputEvent($event.target.value)"
+        >
+      `,
+      methods : {
+        inputEvent(value){
+          this.$emit('input', value)
+        }
+      }
+    },
+  }
+})
+
+//    5) 컴포넌트의 사용자 정의 v-model 
+//       대부분의 컴포넌트의 v-model은 value를 input의 초기값으로 사용하고, 사용자정의이벤트 input을 사용하지만, 체크박스나 라디오버튼과 같은 일부타입은 다른목적으로 value 속성을 사용할 수 있습니다.
+//       model옵션을 사용하면 충돌을 피할 수 있습니다.
+new Vue({
+  el : '#v-model2',
+  data : {
+    boolean : true
+  },
+  computed : {
+    boolcheck(){
+      return this.boolean ? '하겠습니다' : '하지않겠습니다'
+    }
+  },
+  components : {
+    checkbox : {
+      model : {
+        prop : 'checkbool', // v-model로 받는 값
+        event : 'change' // 사용자지정 이벤트는 기본 input이자만 change로 하겠음
+      },
+      props : {
+        checkbool : Boolean,
+      },
+      template : `
+        <input 
+          type="checkbox"
+          :checked="checkbool"
+          @change="change($event.target.checked)"
+        >
+      `,
+      methods : {
+        change(value){
+          this.$emit('change', value)
+        }
+      }
+    }
+  }
+})
+
+//    5) 관련이 없는 컴포넌트간 통신
+//       비어있는 Vue 인스턴스를 통해 중앙 이벤트 버스로 사용할 수 있다.
+//       이벤트를 방출하는 컴포넌트에서 $emit, 받는 컴포넌트에스 $on을 정의해놓는다.
+//       보다 복잡한 경우, 상태관리패턴(vuex?)을 사용해야한다.
+const bus = new Vue()
+new Vue({
+  el : '#exportmethod',
+  methods : {
+    exportMethod(){
+      bus.$emit('exportedEvent', 1)
+    }
+  }
+})
+new Vue({
+  el : '#importmethod',
+  created(){
+    bus.$on('exportedEvent', (num) => {
+      console.log(num)
+    })
+  }
+})
+
+// 3. 슬롯을 사용한 컨텐츠 배포
+//    * 슬롯은 컴포넌트의 재사용성을 높이기 위한 기능이다.
+//    컴포넌트를 사용할 때 다음과 같이 구성하는것이 좋다.
+//    <app>
+//      <app-header></app-header>
+//      <app-footer></app-footer>
+//    </app>
+
+//    1) 범위 컴파일
+//       * 상위 템플릿(DOM)의 모든 내용은 상위 범위에서 컴파일 되며, 하위 템플릿의 모든 내용은 하위 범위에서 컴파일 됩니다.
+//         즉, 컴포넌트에 v-show나 v-if등 디렉티브로 어떤 속성을 묶었을 때, 해당 속성이 상위 범위의 속성이면 작동하지만, 해당 컴포넌트 고유의 속성이면 작동되지 않습니다. 왜냐, 상위범위에서부터 렌더링 하는데, 상위범위는 하위컴포넌트의 속성을 알지 못하기 때문이다.
+
+//    2) 단일슬롯
+//       하위 컴포넌트 템플릿에 최소한 하나의 <slot> 콘텐츠가 포함되어있지 않으면, 부모컨텐츠는 삭제된다.
+//       <slot> 태그 안의 내용은 대체콘텐츠로 간주되며, 대체콘텐츠는 하위 범위에서 컴파일 되고 호스팅 엘리먼트가 비어있어 삽입할 콘텐츠가 없는 경우에만 표시됩니다.
+//       쉽게 정리.
+//       컴포넌트 template에서 <slot>태그는 자신의 부모영역에서 컴포넌트 태그 내부에 컨텐츠를 만들었을 때 해당 컨텐츠들을 유지하기 위해서 존재하는 태그이다.
+//       만약, <slot>태그가 없다면, 부모영역에서 생성된 컨텐츠들은 제거되고, <slot>태그는 있는데, 부모영역에서 생성된 컨텐츠들이 없다면 해당 컴포넌트의 템플릿 내부 <slot>태그의 기본값이 노출된다.(v-if의 경우도 동일 적용됨. 단, v-show는 display : none의 형식이므로, 모두 노출안됨. 슬롯은 부모영역의 컨텐츠가 존재하기때문에 안보이고, 컨텐츠는 그냥 스타일상 안보이기때문)
+new Vue({
+  el : '#slot1',
+  data : {
+    isShow : false
+  },
+  components : {
+    'my-component' : {
+      template : `
+      <div>
+        <h2>나는 자식 컴포넌트의 제목입니다</h2>
+        <slot>
+          제공된 컨텐츠가 없는 경우에만 보실 수 있습니다.
+        </slot>
+      </div>
+      `
+    }
+  }
+})
+
+//    3) 이름을 가지는 슬롯
+//       슬롯 엘리먼트는 특별한 속성인 name을 가지고 있다.
+//       이 속성은 내용을 어떻게 배포해야하는지를 더 설정하는데 사용할 수 있다.
+//       기본적으로 부모 영역에서의 slot속성값과, 컴포넌트의 템플릿에서 슬롯의 name 속성이 같은 부분이 엮이며 name속성이 없다면, 없는 모든부분이 엮이게된다.
+new Vue({
+  el : '#slot2',
+  components : {
+    'my-component' : {
+      template : `
+        <div>
+          <slot name="title">페이지 타이틀이 없습니다.</slot>
+          <slot name="main">메인컨텐츠1이 없습니다</slot>
+          <slot name="banner">중간광고가 없습니다</slot>
+          <slot>메인컨텐츠2이 없습니다</slot>
+          <slot name="footer">연락처 정보가 없습니다/</slot>
+        </div>
+      `
+    }
+  }
+})
+
+//    4) 범위를 가지는 슬롯
+//       이미 렌더링 된 엘리먼트 대신 재사용 가능한 템플릿으로 작동하는 특별한 유형의 슬롯이다. prop을 컴포넌트에게 전달하는 것처럼, 하위 컴포넌트에서 슬롯에게 전달할 수 있다.
+//       부모에게서 특별한 속성 slot-scope(대체되는 slot의 속성을 감지하는 속성)를 가진 템플릿 태그가 있어야 한다.(vue 특성상, 템플릿 태그는 DOM에 노출되지 않음.) slot-scope의 값은, 컴포넌트로부터 전달된 props객체 변수이다.
+//       업데이트 이후, 템플릿태그뿐만아니라 컴포넌트에도 가능하다.
+
+//       쓰는 이유 예상
+//       만약, 재사용하거나, 다른사람에게 배포를 할 때 각기 용도나 사람마다 최종적으로 렌더링을 원하는 데이터의 갯수가 다를 수 있다. 따라서, 해당 컴포넌트 내부의 템플릿에서 가상의 slot을 만들고, 사용자가 원하는 몇개의 데이터를 넣든 매칭된 slot에 값이 들어가도록 하는 것. 만약 이렇게 하지 않는다면, 2개의 태그로 값이 나오길 원하면 템플릿에서도 두개의 태그를 만들어야하는 수동적인 불편함
+
+//       즉. 같은 구조에 사람마다 넣고자하는 데이터의 갯수가 다를수 있기 때문
+
+//      정리.
+//      (1) 부모영역에서 컴포넌트에 순회시킬 값을 보내줌
+//      (2) 해당 컴포넌트에서 보내진 배열 값을 속성으로 받고, 슬롯태그 내부에 v-for순회를 시켜줌. 그리고, :로 순회 된 낱개의 객체를 속성으로 남긴다.
+//      (3) 남겨진 낱개객체속성은, slot으로 대체되는 부모영역의 태그가 slot-scope로 감지할 수 있으며, 형식은 {속성명 : 객체}이런식이다(물론 v-for가 아니라면 그냥 일반 값도 가능).
+//      (4) 따라서, 해당 컴포넌트를 배포할 때, 컴포넌트 내부의 template을 수정할 필요 없이, 부모영역에서 모든 설정 및, 렌더링되는 DOM의 구성을 짤 수 있으므로 재사용성이 높아진다.
+new Vue({
+  el : '#slot3',
+  data : {
+    arr : [
+      {id : 1, text : 'banana'},
+      {id : 2, text : 'apple'},
+      {id : 3, text : 'grapes'},
+    ]
+  },
+  components : {
+    'my-list' : {
+      props : ['items'],
+      template : `
+        <ul>
+          <slot
+            name="item"
+            v-for="item in items"
+            :item="item"
+          >
+          </slot>
+        </ul>
+      `
+    }
+  }
+})
+
+// 4. 동적 컴포넌트
+//    같은 인스턴스 안에 있는 components들을 동적으로 교체할 수 있고, is로 바인드 할 수 있다.
+//    Vue-cli에서 이렇게 할 것 같음
+//    component태그를 keep-alive태그로 감싸서, 동적으로 교체되더라도 재 렌더링하지않고 이전의 값들을 유지할 수 있다.
+new Vue({
+  el : '#activeComponent',
+  data : {
+    currentComp : 'posts'
+  },
+  components : {
+    home : {
+      template : `<p>Hi, I am home</p>`
+    },
+    posts : {
+      template : `<p>Hi, I am posts</p>`
+    },
+    archive : {
+      template : `<p>Hi, I am archive</p>`
+    }
+  }
+})
+
+//  6. 기타
+//     1) 태그의 ref속성과, $refs 호출을 통해 태그를 직접 조회할 수 있다.(document.querySelector처럼)  단, ref는 컴포넌트가 렌더링 된 이후에만 작동되며, 반응적이지 않는다.
